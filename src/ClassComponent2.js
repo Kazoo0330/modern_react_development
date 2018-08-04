9f6c0dded7d3018d6259de9f9f66d5a0ee0ecdee
kazoo <kazuki_saito@diveintocode.jp>
2018-08-04 11:55:24 +0900

    feat: lecture14 completed "export default usage"

---
+++ src/ClassComponent2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// ここで一旦 class の定義
+class ClassComponent2 extends React.Component {
+  render() {
+    return <div>ClassComponent2</div>;
+  }
+}
+
+// ここでは上で定義した class をexport defaultする。
+export default ClassComponent2;
